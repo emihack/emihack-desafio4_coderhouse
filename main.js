eb3d3f34094732a5f6ec560eb7cd567f8a718dc1
Hermes Emiliano Rodriguez <emirodriguez@icloud.com>
2022-03-03 11:05:43 -0300

    creando repo

---
+++ main.js
@@ -0,0 +1,39 @@
+let destino = prompt ("¿Hacia donde vas a ir?").toUpperCase();
+
+let presupuesto = parseInt( prompt ("¿Cuanto dispones para destinarle a los gastos del vehículo?"));
+
+let combustible = parseInt(prompt("Cuanto dinero esta destinado al combustible?")) ;
+
+let comida = parseInt(prompt ("Cuanto dinero tenes destinado para la comida?")) ;
+
+let seguro = parseInt(prompt ("Cuanto dinero tenes destinado al seguro?")) ;
+
+let gastos = parseInt(seguro) + parseInt (combustible) + parseInt (comida);
+
+suma(seguro, combustible, comida);
+
+resta(presupuesto, gastos);
+
+function suma (seguro, combustible, comida){
+
+    let gastos = parseInt(seguro) + parseInt (combustible) + parseInt (comida);
+
+}
+
+function resta (presupuesto, gastos){
+    
+    let balance = parseInt(presupuesto) - parseInt (gastos) ;
+
+
+    if(balance > 0){
+        document.write("Tenes el dinero suficiente para realizar el viaje" + "<br>");
+        alert("Buenisimo te alcanza para irte en tu vehículo pasala fantástico!");
+    }
+    else if(balance < 0 ){
+        alert("De mochila es mas barato");
+    }
+    else {
+        alert("Si vas en colectivo es mas barato");
+    }    
+
+}
